48722663c788865e86a80c688b52eab994ef50e6
aein <aeinnor_reyes@dlsu.edu.ph>
2022-10-21 21:33:37 +0800

    Testing Units for Merge Sort
    This commit contains code required for statistical unit testing.

--- Sort/src/Driver.java
+++ Sort/src/Driver.java
@@ -7,11 +7,12 @@ public class Driver {
 
         String[] testArray = new String[numTests];
 
+        System.out.println("Test String");
         for (int i = 0; i < numTests; i++) {
-                testArray[i] = StringRandomizer.Generate(Size, chars);
+            testArray[i] = StringRandomizer.Generate(Size, chars);
 
-                // print all the strings
-                System.out.println(testArray[i]);
+//            // print all the strings
+//            System.out.println(i + ": " + testArray[i]);
         }
 
         long startTime = 0;
@@ -33,7 +34,7 @@ public class Driver {
 
             while (left < end) {
                 sArray[left] = input.substring(left, end);
-//                System.out.println(left + ": " + sArray[left]);
+                System.out.println(left + ": " + sArray[left]);
                 left++;
             }
 
@@ -42,11 +43,10 @@ public class Driver {
             endTime = System.nanoTime();
 
             System.out.println("TEST " + i);
-            for(String s : sArray) {
-                System.out.println(s);
+            for(int x = 0; x < sArray.length; x++) {
+                System.out.println(suffixArray[x] + ": " + sArray[x]);
             }
             System.out.println();
-
             totalTime = endTime - startTime;
             times[i] = totalTime;
 
@@ -65,15 +65,16 @@ public class Driver {
 
     public static void testMergeSort(int Size, int numTests, final String chars) {
 
-        MergeSort mergeSort = new MergeSort();
+        MergeSort mergeSort = new MergeSort(
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
 
         String[] testArray = new String[numTests];
 
         for (int i = 0; i < numTests; i++) {
             testArray[i] = StringRandomizer.Generate(Size, chars);
 
-            // print all the strings
-            System.out.println(testArray[i]);
+//            // print all the strings
+//            System.out.println(i + ": " + testArray[i]);
         }
 
         long startTime = 0;
@@ -95,7 +96,7 @@ public class Driver {
 
             while (left < end) {
                 sArray[left] = input.substring(left, end);
-//                System.out.println(left + ": " + sArray[left]);
+                System.out.println(left + ": " + sArray[left]);
                 left++;
             }
 
@@ -104,8 +105,8 @@ public class Driver {
             endTime = System.nanoTime();
 
             System.out.println("TEST " + i);
-            for(String s : sArray) {
-                System.out.println(s);
+            for(int x = 0; x < sArray.length; x++) {
+                System.out.println(suffixArray[x] + ": " + sArray[x]);
             }
             System.out.println();
 
@@ -123,93 +124,35 @@ public class Driver {
         }
 
         System.out.println("average time: " + averageTime/1000000.0 + " ms");
+
+        // print all counts
+        System.out.println("Merge Sort ifCount: " + mergeSort.ifCount);
+        System.out.println("Merge Sort midCount: " + mergeSort.midCount);
+        System.out.println("Merge Sort mergeSort1: " + mergeSort.mergeCount1);
+        System.out.println("Merge Sort mergeSort2: " + mergeSort.mergeCount2);
+        System.out.println("Merge Sort mergeCount " + mergeSort.merging);
+//
+        System.out.println("MERGE COUNTS");
+        System.out.println("Merge While Count: " + mergeSort.mergeWhile);
+        System.out.println("ifCount: " + mergeSort.mergeif1);
+        System.out.println("Swap A1: " + mergeSort.swapA1);
+        System.out.println("Swap B1: " + mergeSort.swapB1);
+        System.out.println("Swap jIncrement: " + mergeSort.jIncrement);
+        System.out.println("Merge While Count2: " + mergeSort.mergeWhile2);
+        System.out.println("Swap C1: " + mergeSort.swapC1);
+        System.out.println("Merge While Count3: " + mergeSort.mergeWhile3);
+        System.out.println("Swap D1: " + mergeSort.swapD1);
+        System.out.println("LastforCount: " + mergeSort.lastFor);
+        System.out.println("Swap D2: " + mergeSort.swapD2);
     }
 
     public static void main (String[] args) {
 
-        testInsertion(128, 10, "acgt");
-        testMergeSort(128, 10, "acgt");
-
-//        Scanner scanner = new Scanner(System.in);
-//
-//        String input;
-//
-//        System.out.print("Input string: ");
-//        input = StringRandomizer.Generate(1024, "acgt");
-//
-//        // Generate Substrings
-//        int left = 0;
-//        int end = input.length();
-//        String[] sArray = new String[end];
-//        String[] values = new String[end];
-//        int[] suffixArray = new int[sArray.length];
-//
-//        System.out.println();
-//        System.out.println("Suffix Array:");
-//
-//        while (left < end) {
-//            sArray[left] = input.substring(left, end);
-//            System.out.println(left + ": " + sArray[left]);
-//            left++;
-//        }
-//
-//        // Modified Insertion Sort
-//        InsertionSort InsertionSort = new InsertionSort();
-//
-//        long startTime = System.nanoTime();
-//
-//        InsertionSort.insertionSort(sArray, suffixArray);
-//
-//        long endTime = System.nanoTime();
-//        long duration = (endTime - startTime);
-//        System.out.println("Insertion Sort Time: " + duration/1000000 + "ms");
-//
-//        System.out.println();
 //        System.out.println("Insertion Sort:");
-//
-//        for (int i = 0; i < sArray.length; i++) {
-//            System.out.println(suffixArray[i] +": "+ sArray[i]);
-//        }
+//        testInsertion(10, 1, "acgt");
 
-        // Modified Merge Sort
-//        left = 0;
-//
-//        while (left < end){
-//            values[left] = input.substring(left, end);
-//            left++;
-//        }
-//        left = 0;
+        System.out.println("\nMerge Sort:");
+        testMergeSort(10000, 1, "acgt");
 
-//        MergeSort MergeSort = new MergeSort();
-//        long startTime = System.nanoTime();
-//
-//        MergeSort.mergeSort(sArray, 0, sArray.length - 1, suffixArray);
-//
-//        long endTime = System.nanoTime();
-//        long duration = (endTime - startTime);
-//        System.out.println(duration/1000000.0 + " ms");
-
-
-
-
-
-
-
-
-        // print all elements of kvp
-
-
-
-
-//        MergeSort.mergeSort(kvp, 0, sArray.length - 1);
-
-
-//        System.out.println();
-//        System.out.println("Merge Sort:");
-//        for (int p = 0; p < sArray.length; p++) {
-//            System.out.println(suffixArray[p] +": "+ values[p]);
-//        }
-
-        System.out.println(System.nanoTime() / 1000000);
     }
 }
--- Sort/src/MergeSort.java
+++ Sort/src/MergeSort.java
@@ -1,11 +1,99 @@
 // Merge sort coding implementation
 public class MergeSort {
 
+ public     int ifCount;
+  public  int midCount;
+   public int mergeCount1;
+    public int mergeCount2;
+
+
+
+    public int mergeWhile;
+    public int mergeif1;
+    public int swapA1, swapA2, swapA3;
+    public int swapB1, swapB2, swapB3;
+    public int jIncrement;
+    public int mergeWhile2;
+    public int swapC1, swapC2, swapC3;
+
+    public int swapD1, swapD2, swapD3;
+    public int jIncrement2;
+    public int mergeWhile3;
+
+    public int lastFor;
+
+
+    public int merging;
+
+    public MergeSort(
+        int ifCount,
+        int midCount,
+        int mergeCount1,
+        int mergeCount2,
+        int mergeWhile,
+        int mergeif1,
+        int swapA1,
+        int swapA2,
+        int swapA3,
+        int swapB1,
+        int swapB2,
+        int swapB3,
+        int jIncrement,
+        int mergeWhile2,
+        int swapC1,
+        int swapC2,
+        int swapC3,
+        int jIncrement2,
+        int mergeWhile3,
+        int merging,
+        int swapD1,
+        int swapD2,
+        int swapD3,
+        int lastFor
+    ) {
+        this.ifCount = ifCount;
+        this.midCount = midCount;
+        this.mergeCount1 = mergeCount1;
+        this.mergeCount2 = mergeCount2;
+        this.mergeWhile = mergeWhile;
+        this.mergeif1 = mergeif1;
+        this.swapA1 = swapA1;
+        this.swapA2 = swapA2;
+        this.swapA3 = swapA3;
+        this.swapB1 = swapB1;
+        this.swapB2 = swapB2;
+        this.swapB3 = swapB3;
+        this.jIncrement = jIncrement;
+        this.mergeWhile2 = mergeWhile2;
+        this.swapC1 = swapC1;
+        this.swapC2 = swapC2;
+        this.swapC3 = swapC3;
+        this.jIncrement2 = jIncrement2;
+        this.mergeWhile3 = mergeWhile3;
+        this.merging = merging;
+        this.swapD1 = swapD1;
+        this.swapD2 = swapD2;
+        this.swapD3 = swapD3;
+        this.lastFor = lastFor;
+
+        this.swapD2 = 0;
+    }
+
+
     public void mergeSort(String[] strings, int left, int right, int[] suffixArray) {
+        ifCount++;
         if (left < right) {
+            midCount++;
+
             int middle = (left + right) / 2;
+
+            mergeCount1++;
             mergeSort(strings, left, middle, suffixArray);
+
+            mergeCount2++;
             mergeSort(strings, middle + 1, right, suffixArray);
+
+            merging++;
             merge(strings, left, middle, right, suffixArray);
         }
     }
@@ -17,98 +105,49 @@ public class MergeSort {
         int i2 = mid + 1;
         int j = 0;
 
+        mergeWhile++;
         while (i1 <= mid && i2 <= right) {
+            mergeif1++;
             if (a[i1].compareTo(a[i2]) < 0) {
+                swapA1++;
                 b[j] = a[i1];
                 suffixArray[j] = i1;
                 i1++;
+
             } else {
+                swapB1++;
                 b[j] = a[i2];
                 suffixArray[j] = i2;
                 i2++;
             }
+            jIncrement++;
             j++;
         }
 
+        mergeWhile2++;
         while (i1 <= mid) {
+            swapC1++;
             b[j] = a[i1];
             suffixArray[j] = i1;
             i1++;
             j++;
         }
 
+        mergeWhile3++;
         while (i2 <= right) {
+            swapD1++;
             b[j] = a[i2];
             suffixArray[j] = i2;
             i2++;
             j++;
         }
 
+        lastFor++;
+
         for (j = 0; j < n; j++) {
+            swapD2++;
             a[left + j] = b[j];
         }
     }
 }
-
-// merge method for mergeSort
-
-
-// merge method for mergeSort
-// keys must be swapp
-
-
-//        // combine suffixes in SuffixArray
-//        for (int x = 0; x < leftSuffixes.length; x++) {
-//            SuffixArray[x] = leftSuffixes[x];
-//        }
-//        for (int y = 0; y < rightSuffixes.length; y++) {
-//            SuffixArray[y + leftSuffixes.length] = rightSuffixes[y];
-//        }
-
-
-//    public void merge(String[] values, int[] suffixArray, int[] temp, int left, int mid, int right) {
-//        int i = left;
-//        int j = mid;
-//        int k = left;
-//
-//        // Compares the two strings and swaps indices if applicable
-//        // Updates the suffix array
-//        while (i < mid && j < right) {
-//            if (values[i].compareTo(values[j]) < 0) {
-//                temp[k] = suffixArray[i];
-//                values[k++] = values[i++];
-//            } else {
-//                temp[k] = suffixArray[j];
-//                values[k++] = values[j++];
-//            }
-//        }
-//
-//        // Copies the remaining elements of the left array
-//        while (i < mid) {
-//            temp[k] = suffixArray[i];
-//            values[k++] = values[i++];
-//        }
-//
-//        // Copies the remaining elements of the right array
-//        while (j < right) {
-//            temp[k] = suffixArray[j];
-//            values[k++] = values[j++];
-//        }
-//
-//        // Copies the elements from the temp array to the suffix array
-//        for (i = left; i < right; i++) {
-//            suffixArray[i] = temp[i];
-//        }
-//    }
-
-//    public void mergeSort(String[] values, int left, int right) {
-//        if (left < right) {
-//            int middle = (left + right) / 2;
-//            mergeSort(values, left, middle);
-//            mergeSort(values, middle + 1, right);
-//            merge(values, left, middle, right);
-//        }
-//    }
-//
-
 
